047012a9ada580991d54645408f206acfdac6c2e
Waitara <janewaitara99@gmail.com>
2022-07-21 23:40:59 +0300

    Migrated typography scale to use Material 3

--- app/src/main/java/com/mumbicodes/ui/theme/Type.kt
+++ app/src/main/java/com/mumbicodes/ui/theme/Type.kt
@@ -1,18 +1,16 @@
 package com.mumbicodes.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import com.mumbicodes.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     //This is the Title
-    h5 = TextStyle(
+    displayMedium = TextStyle(
         fontFamily = fontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 32.sp,
@@ -21,7 +19,7 @@ val Typography = Typography(
     ),
 
     //This is the Header 2
-    h6= TextStyle(
+    headlineMedium= TextStyle(
         fontFamily = fontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
@@ -30,28 +28,28 @@ val Typography = Typography(
     ) ,
 
     //This is the subtitle
-    subtitle1 = TextStyle(
+    titleMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
         lineHeight = 28.sp
     ),
     //This is the body
-    body1 = TextStyle(
+    bodySmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp
     ),
     //This is the strong body in my guide
-    body2 = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp
     ),
     //This is the button text
-    button = TextStyle(
+    labelLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp,
@@ -59,15 +57,15 @@ val Typography = Typography(
         lineHeight = 20.sp
     ),
     //This is the small text in my guide
-    caption = TextStyle(
+    labelMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W400,
         fontSize = 14.sp,
         letterSpacing =0.01.em,
         lineHeight = 20.sp
     ),
-    //This is the small text in my guide
-    overline = TextStyle(
+    //This is the smallest text in my guide
+    labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W400,
         fontSize = 12.sp,
