bd360f533b695370f8af947b7b318a2d229292d1
Waitara <janewaitara99@gmail.com>
2022-08-20 18:57:56 +0300

    Added a projects repository interface

---
+++ app/src/main/java/com/mumbicodes/domain/repository/ProjectsRepository.kt
@@ -0,0 +1,21 @@
+package com.mumbicodes.domain.repository
+
+import com.mumbicodes.domain.model.Project
+import kotlinx.coroutines.flow.Flow
+
+interface ProjectsRepository {
+
+    suspend fun insertProject(project: Project)
+
+    fun getProjects(): Flow<List<Project>>
+
+    suspend fun getProjectById(projectId: Int): Project
+
+    fun getAllProjectsBasedOnStatus(projectStatus: String?): Flow<List<Project>>
+
+    fun getProjectsBasedOnDueDate(projectStatus: String?): Flow<List<Project>>
+
+    suspend fun deleteProject(project: Project)
+
+    suspend fun deleteAllProjects()
+}
