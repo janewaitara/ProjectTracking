d075918e5ba4d6d93668be62841c9d2081152b68
Waitara <janewaitara99@gmail.com>
2022-08-27 15:56:59 +0300

    Updated get Project by Id use case to return a project

--- app/src/main/java/com/mumbicodes/domain/use_case/projects/GetProjectByIdUseCase.kt
+++ app/src/main/java/com/mumbicodes/domain/use_case/projects/GetProjectByIdUseCase.kt
@@ -1,11 +1,11 @@
 package com.mumbicodes.domain.use_case.projects
 
+import com.mumbicodes.domain.model.Project
 import com.mumbicodes.domain.repository.ProjectsRepository
 
 class GetProjectByIdUseCase(
     private val repository: ProjectsRepository
 ) {
-    suspend operator fun invoke(projectId: Int) {
+    suspend operator fun invoke(projectId: Int): Project =
         repository.getProjectById(projectId)
-    }
 }
