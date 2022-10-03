d59e475187e53d89c6fe56ab63a17263e5a10368
Waitara <janewaitara99@gmail.com>
2022-10-03 19:59:08 +0300

    Updated the one to many relation for project with milestones

--- app/src/main/java/com/mumbicodes/domain/relations/ProjectWithMilestones.kt
+++ app/src/main/java/com/mumbicodes/domain/relations/ProjectWithMilestones.kt
@@ -2,7 +2,6 @@ package com.mumbicodes.domain.relations
 
 import androidx.room.Embedded
 import androidx.room.Relation
-import com.mumbicodes.domain.model.Milestone
 import com.mumbicodes.domain.model.Project
 
 data class ProjectWithMilestones(
@@ -11,5 +10,5 @@ data class ProjectWithMilestones(
         parentColumn = "projectId",
         entityColumn = "projectId"
     )
-    val milestone: List<Milestone>
+    val milestone: List<MilestoneWithTasks>
 )
