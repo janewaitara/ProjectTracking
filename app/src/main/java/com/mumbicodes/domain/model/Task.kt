4133a88e6e3f14b348113d71fb25819712db5839
Waitara <janewaitara99@gmail.com>
2022-10-03 19:18:06 +0300

    Updated task entity to have a foreignKey

--- app/src/main/java/com/mumbicodes/domain/model/Task.kt
+++ app/src/main/java/com/mumbicodes/domain/model/Task.kt
@@ -1,9 +1,22 @@
 package com.mumbicodes.domain.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tasks_table")
+@Entity(
+    tableName = "tasks_table",
+    foreignKeys = [
+        ForeignKey(
+            entity = Milestone::class,
+            parentColumns = ["milestoneId"],
+            childColumns = ["milestoneId"],
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.CASCADE,
+        )
+    ]
+)
+
 data class Task(
     val milestoneId: Int,
     @PrimaryKey(autoGenerate = false)
