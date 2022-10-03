960641135aa3c360431ff0c76147cf9aafe8777c
Waitara <janewaitara99@gmail.com>
2022-10-04 01:02:03 +0300

    Updated the project with Milestone relation

--- app/src/main/java/com/mumbicodes/domain/relations/ProjectWithMilestones.kt
+++ app/src/main/java/com/mumbicodes/domain/relations/ProjectWithMilestones.kt
@@ -2,11 +2,13 @@ package com.mumbicodes.domain.relations
 
 import androidx.room.Embedded
 import androidx.room.Relation
+import com.mumbicodes.domain.model.Milestone
 import com.mumbicodes.domain.model.Project
 
 data class ProjectWithMilestones(
     @Embedded val project: Project,
     @Relation(
+        entity = Milestone::class,
         parentColumn = "projectId",
         entityColumn = "projectId"
     )
