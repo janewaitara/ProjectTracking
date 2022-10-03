5a8294a4d5b4026e46b572560dffa6c9b12ba40f
Waitara <janewaitara99@gmail.com>
2022-10-04 01:10:23 +0300

    Added tasks repository and it's implementation

--- app/src/main/java/com/mumbicodes/di/RepositoryModule.kt
+++ app/src/main/java/com/mumbicodes/di/RepositoryModule.kt
@@ -3,8 +3,10 @@ package com.mumbicodes.di
 import com.mumbicodes.data.db.ProjectsDatabase
 import com.mumbicodes.data.repository.MilestonesRepositoryImpl
 import com.mumbicodes.data.repository.ProjectsRepositoryImpl
+import com.mumbicodes.data.repository.TaskRepositoryImpl
 import com.mumbicodes.domain.repository.MilestonesRepository
 import com.mumbicodes.domain.repository.ProjectsRepository
+import com.mumbicodes.domain.repository.TasksRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -25,5 +27,11 @@ object RepositoryModule {
     @Singleton
     fun provideMilestonesRepository(db: ProjectsDatabase): MilestonesRepository {
         return MilestonesRepositoryImpl(db.milestonesDao())
+    }
+
+    @Provides
+    @Singleton
+    fun provideTasksRepository(db: ProjectsDatabase): TasksRepository {
+        return TaskRepositoryImpl(db.tasksDao())
     }
 }
