78519b4699b0c3e2b62ca07890c3be3ee1394cab
Waitara <janewaitara99@gmail.com>
2023-01-11 10:47:18 +0300

    Updated the statusBar colors

--- app/src/main/java/com/mumbicodes/presentation/theme/Theme.kt
+++ app/src/main/java/com/mumbicodes/presentation/theme/Theme.kt
@@ -81,7 +81,7 @@ fun ProjectTrackingTheme(
         SideEffect {
             val window = (view.context as Activity).window
             val insets = WindowCompat.getInsetsController(window, view)
-            window.statusBarColor = LightColorPalette.surface.toArgb() // choose a status bar color
+            window.statusBarColor = if (darkTheme) DarkColorPalette.background.toArgb() else LightColorPalette.background.toArgb() // choose a status bar color
             // window.navigationBarColor = LightColorPalette.surface.toArgb() // choose a navigation bar color
             insets.isAppearanceLightStatusBars = !darkTheme
             // insets.isAppearanceLightNavigationBars = !darkTheme
