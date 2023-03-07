5e3a72456e81518f0440d221028b89e3899f5564
Waitara <janewaitara99@gmail.com>
2023-03-07 18:55:44 +0300

    Added a WorkerRepository

--- app/src/main/java/com/mumbicodes/projectie/presentation/util/Constants.kt
+++ app/src/main/java/com/mumbicodes/projectie/presentation/util/Constants.kt
@@ -5,3 +5,4 @@ const val MILESTONE_ID = "MILESTONE_ID"
 
 // Milestone Worker
 const val KEY_ENDING_MILESTONES = "KEY_ENDING_MILESTONES"
+const val PROJECTS_DEADLINE_WORK_NAME = "milestones_deadline_worker"
