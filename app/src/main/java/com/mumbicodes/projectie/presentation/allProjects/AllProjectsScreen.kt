4af1dee4215b19b480a035592d9070e56914e5a5
Waitara <janewaitara99@gmail.com>
2023-03-13 21:26:48 +0300

    Added logic to ask for permission in API 33

--- app/src/main/java/com/mumbicodes/projectie/presentation/allProjects/AllProjectsScreen.kt
+++ app/src/main/java/com/mumbicodes/projectie/presentation/allProjects/AllProjectsScreen.kt
@@ -1,6 +1,11 @@
 package com.mumbicodes.projectie.presentation.allProjects
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -10,19 +15,18 @@ import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material3.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
@@ -32,7 +36,9 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.permissions.*
 import com.mumbicodes.projectie.R
 import com.mumbicodes.projectie.domain.model.Project
 import com.mumbicodes.projectie.domain.util.OrderType
@@ -42,14 +48,12 @@ import com.mumbicodes.projectie.presentation.allProjects.components.FilterBottom
 import com.mumbicodes.projectie.presentation.allProjects.components.ProjectItem
 import com.mumbicodes.projectie.presentation.allProjects.components.SearchBar
 import com.mumbicodes.projectie.presentation.allProjects.components.StaggeredVerticalGrid
-import com.mumbicodes.projectie.presentation.components.EmptyStateSlot
-import com.mumbicodes.projectie.presentation.components.ErrorStateSlot
-import com.mumbicodes.projectie.presentation.components.FilterChip
-import com.mumbicodes.projectie.presentation.components.ShimmerEffectComposable
+import com.mumbicodes.projectie.presentation.components.*
 import com.mumbicodes.projectie.presentation.theme.*
 import com.mumbicodes.projectie.presentation.util.ReferenceDevices
 import kotlinx.coroutines.launch
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun AllProjectsScreen(
@@ -72,6 +76,35 @@ fun AllProjectsScreen(
 
     BackHandler(modalBottomSheetState.isVisible) {
         scope.launch { modalBottomSheetState.hide() }
+    }
+
+    val context = LocalContext.current
+    var hasNotificationPermission by remember {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            mutableStateOf(
+                ContextCompat.checkSelfPermission(
+                    context,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+        } else mutableStateOf(true)
+    }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            hasNotificationPermission = isGranted
+        }
+    )
+    LaunchedEffect(key1 = true) {
+        if (!hasNotificationPermission) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                /**
+                 * Request for notification permission on API 33
+                 */
+                permissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 
     ModalBottomSheetLayout(
